5832b9e1a7f5225ee4c9064e0df100119c38b2c1
liza <elizabeth1204jjj@gmail.com>
2024-01-31 00:00:54 +0200

    implement filters

---
+++ src/main/java/com/example/petfinder/model/enums/Age.java
@@ -0,0 +1,34 @@
+package com.example.petfinder.model.enums;
+
+public enum Age {
+
+    LESS_THAN_1("<1"),
+    ONE("1"),
+    TWO("2"),
+    THREE("3"),
+    FOUR("4"),
+    FIVE("5"),
+    SIX("6"),
+    SEVEN("7"),
+    EIGHT("8"),
+    NINE("9"),
+    TEN("10"),
+    ELEVEN("11"),
+    TWELVE("12"),
+    THIRTEEN("13"),
+    FOURTEEN("14"),
+    FIFTEEN("15"),
+    SIXTEEN("16"),
+    SEVENTEEN("17"),
+    EIGHTEEN("18"),
+    NINETEEN("19");
+    private final String value;
+
+    Age(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
